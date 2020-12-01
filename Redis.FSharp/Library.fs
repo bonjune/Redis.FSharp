8d696be01cfc21dce9ae64e05f756f8d84c1eeea
Bonjune <malloc099@gmail.com>
2020-12-02 00:54:38 +0900

    feat : implement some more wrappers

--- Redis.FSharp/Library.fs
+++ Redis.FSharp/Library.fs
@@ -7,23 +7,52 @@ type RedisConnectionBuilder =
 
 [<RequireQualifiedAccess>]
 module Redis =
-    (* Connections*)
+    (* Connections *)
     let init =
         { ConnectionString = "" }
+    
+    /// https://stackexchange.github.io/StackExchange.Redis/Configuration
+    let options configString =
+        ConfigurationOptions.Parse configString
     
     let connString connString builder =
         { builder with ConnectionString = connString }
 
+    /// Caution: do not make multiple multiplexers! Just re-use it!
     let connect () builder =
         ConnectionMultiplexer.Connect builder.ConnectionString
+    
+    let server host port (multiplexer : ConnectionMultiplexer) =
+        multiplexer.GetServer(host, port)
+    
+    let endpoints (multiplexer : ConnectionMultiplexer) =
+        multiplexer.GetEndPoints()
+    
+    let clients (server : IServer) =
+        server.ClientList()
 
 
     (* Database *)
     let db (multiplexer : ConnectionMultiplexer) =
         multiplexer.GetDatabase()
     
+    let stringSet (key: RedisKey) (value: RedisValue) (db : IDatabase) =
+        db.StringSet(key, value)
+    
+    let stringSetAsync (key: RedisKey) (value: RedisValue) (db : IDatabase) =
+        db.StringSetAsync(key, value)
+
+    let stringGet (key : RedisKey) (db : IDatabase) =
+        db.StringGet key
+    
+    let stringGetAsync (key : RedisKey) (db : IDatabase) =
+        db.StringGetAsync key
+
+    let stringGetWithFlag (key : RedisKey) flag (db : IDatabase) =
+        db.StringGet(key, flag)
+
     (* Pub/Sub *)
-    let subscribder (multiplexer : ConnectionMultiplexer) =
+    let subscriber (multiplexer : ConnectionMultiplexer) =
         multiplexer.GetSubscriber()
     
     let subscribe channel observable (sub : ISubscriber) =

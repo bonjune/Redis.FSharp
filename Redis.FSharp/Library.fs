f11612bb822da855c1d647c32602b2e3ac157c15
Bonjune <malloc099@gmail.com>
2020-12-02 00:38:27 +0900

    project : create gitignore file

---
+++ Redis.FSharp/Library.fs
@@ -0,0 +1,37 @@
+﻿namespace Redis.FSharp
+
+open StackExchange.Redis
+
+type RedisConnectionBuilder =
+    { ConnectionString : string }
+
+[<RequireQualifiedAccess>]
+module Redis =
+    (* Connections*)
+    let init =
+        { ConnectionString = "" }
+    
+    let connString connString builder =
+        { builder with ConnectionString = connString }
+
+    let connect () builder =
+        ConnectionMultiplexer.Connect builder.ConnectionString
+
+
+    (* Database *)
+    let db (multiplexer : ConnectionMultiplexer) =
+        multiplexer.GetDatabase()
+    
+    (* Pub/Sub *)
+    let subscribder (multiplexer : ConnectionMultiplexer) =
+        multiplexer.GetSubscriber()
+    
+    let subscribe channel observable (sub : ISubscriber) =
+        sub.Subscribe(channel, observable)
+    
+    let publish channel msg (sub : ISubscriber) =
+        sub.Publish(channel, msg)
+
+    let publishWithFlag channel msg flag (sub : ISubscriber) =
+        sub.Publish(channel, msg, flag)
+

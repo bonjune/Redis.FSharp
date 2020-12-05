bd3f2cc45170de76a33369a11e0a7bf3193a3f04
Bonjune <malloc099@gmail.com>
2020-12-05 14:39:09 +0900

    add option types for returning nil and some functions

--- Redis.FSharp/Library.fs
+++ Redis.FSharp/Library.fs
@@ -1,66 +1,96 @@
 ﻿namespace Redis.FSharp
 
 open StackExchange.Redis
+open FSharp.Control.Tasks.V2.ContextInsensitive
 
-type RedisConnectionBuilder =
-    { ConnectionString : string }
+type RedisConnectionBuilder = { ConnectionString : string }
+
+module RedisValue =
+    let toOption (value : RedisValue) =
+        if value.IsNullOrEmpty then None else Some value
 
 [<RequireQualifiedAccess>]
 module Redis =
     (* Connections *)
-    let init =
-        { ConnectionString = "" }
-    
+    let init = { ConnectionString = "" }
+
     /// https://stackexchange.github.io/StackExchange.Redis/Configuration
-    let options configString =
+    let configOptsFromString configString =
         ConfigurationOptions.Parse configString
-    
+
     let connString connString builder =
         { builder with ConnectionString = connString }
 
     /// Caution: do not make multiple multiplexers! Just re-use it!
     let connect () builder =
         ConnectionMultiplexer.Connect builder.ConnectionString
-    
+
     let server host port (multiplexer : ConnectionMultiplexer) =
         multiplexer.GetServer(host, port)
-    
+
     let endpoints (multiplexer : ConnectionMultiplexer) =
         multiplexer.GetEndPoints()
-    
-    let clients (server : IServer) =
-        server.ClientList()
 
+    let clients (server : IServer) = server.ClientList()
 
     (* Database *)
+
     let db (multiplexer : ConnectionMultiplexer) =
         multiplexer.GetDatabase()
-    
-    let stringSet (key: RedisKey) (value: RedisValue) (db : IDatabase) =
+
+    let stringSet (key : RedisKey) (value : RedisValue) (db : IDatabase) =
         db.StringSet(key, value)
-    
-    let stringSetAsync (key: RedisKey) (value: RedisValue) (db : IDatabase) =
+
+    let stringSetAsync (key : RedisKey) (value : RedisValue) (db : IDatabase) =
         db.StringSetAsync(key, value)
 
     let stringGet (key : RedisKey) (db : IDatabase) =
-        db.StringGet key
-    
+        db.StringGet key |> RedisValue.toOption
+
     let stringGetAsync (key : RedisKey) (db : IDatabase) =
-        db.StringGetAsync key
+        task {
+            let! value = db.StringGetAsync key
+            return RedisValue.toOption value
+        }
 
     let stringGetWithFlag (key : RedisKey) flag (db : IDatabase) =
-        db.StringGet(key, flag)
+        db.StringGet(key, flag) |> RedisValue.toOption
+
+    let keyDelete (key : RedisKey) (db : IDatabase) = db.KeyDelete(key)
+
+    let keyDeleteWithFlag (key : RedisKey) flag (db : IDatabase) =
+        db.KeyDelete(key, flag)
+
+    let keyDeleteMany (keys : RedisKey []) (db : IDatabase) = db.KeyDelete(keys)
+
+    let keyDeleteManyWithFlag (keys : RedisKey []) flag (db : IDatabase) =
+        db.KeyDelete(keys, flag)
+
+    let keyDeleteAsync (key : RedisKey) (db : IDatabase) =
+        db.KeyDeleteAsync(key)
+
+    let keyDeleteWithFlagAsync (key : RedisKey) flag (db : IDatabase) =
+        db.KeyDeleteAsync(key, flag)
+
+    let keyDeleteManyAsync (keys : RedisKey []) (db : IDatabase) =
+        db.KeyDeleteAsync(keys)
+
+    let keyDeleteManyWithFlagAsync (keys : RedisKey []) flag (db : IDatabase) =
+        db.KeyDeleteAsync(keys, flag)
 
     (* Pub/Sub *)
+
     let subscriber (multiplexer : ConnectionMultiplexer) =
         multiplexer.GetSubscriber()
-    
-    let subscribe channel observable (sub : ISubscriber) =
-        sub.Subscribe(channel, observable)
-    
+
+    let subscribe channel (continuation : RedisChannel -> RedisValue -> unit) (sub : ISubscriber) =
+        sub.Subscribe(channel, continuation)
+
+    let subscribeWithFlag channel continuation flag (sub : ISubscriber) =
+        sub.Subscribe(channel, continuation, flag)
+
     let publish channel msg (sub : ISubscriber) =
         sub.Publish(channel, msg)
 
     let publishWithFlag channel msg flag (sub : ISubscriber) =
         sub.Publish(channel, msg, flag)
-
